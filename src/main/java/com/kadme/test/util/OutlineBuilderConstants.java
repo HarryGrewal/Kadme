328d297334e01c10704edde82d738a2bc8bdda5e
Harbans Grewal <harbans_grewal@hotmail.com>
2020-11-25 05:11:55 +0100

    fixed bugs

--- src/main/java/com/kadme/test/util/OutlineBuilderConstants.java
+++ src/main/java/com/kadme/test/util/OutlineBuilderConstants.java
@@ -49,7 +49,7 @@ public final class OutlineBuilderConstants {
         add(new Line(new Point(185, 50), new Point(190, 450)));
         add(new Line(new Point(190, 50), new Point(195, 450)));
         add(new Line(new Point(195, 50), new Point(200, 450)));
-        add(new Line(new Point(200, 50), new Point(250, 450)));
+        add(new Line(new Point(200, 50), new Point(205, 450)));
     }};
 
     private OutlineBuilderConstants() {
